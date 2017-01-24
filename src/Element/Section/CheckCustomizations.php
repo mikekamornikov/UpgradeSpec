76690da5455cafb77bcae3a91bd6b9c546a42a4f
dstiblo <dstiblo@sugarcrm.com>
2017-01-24 18:08:51 +0300

    init check customization step

---
+++ src/Element/Section/CheckCustomizations.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Sugarcrm\UpgradeSpec\Element\Section;
+
+use Sugarcrm\UpgradeSpec\Data\DataAwareInterface;
+use Sugarcrm\UpgradeSpec\Data\DataAwareTrait;
+use Sugarcrm\UpgradeSpec\Element\ElementInterface;
+use Sugarcrm\UpgradeSpec\Spec\Context;
+use Sugarcrm\UpgradeSpec\Template\RendererAwareInterface;
+use Sugarcrm\UpgradeSpec\Template\RendererAwareTrait;
+
+class CheckCustomizations implements ElementInterface, RendererAwareInterface, DataAwareInterface
+{
+    use RendererAwareTrait, DataAwareTrait;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return 'Analyze customizations';
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 4;
+    }
+
+    /**
+     * @param Context $context
+     *
+     * @return bool
+     */
+    public static function isRelevantTo(Context $context)
+    {
+        return true;
+    }
+
+    /**
+     * @param Context $context
+     *
+     * @return string
+     */
+    public function getBody(Context $context)
+    {
+        return $this->renderer->render('check_customizations', [
+            'release_notes' => $this->dataManager->getReleaseNotes($context),
+        ]);
+    }
+}
