e2784f48500478253bff4cbaf8a9c6235171a41b
dstiblo <dstiblo@sugarcrm.com>
2017-01-24 19:44:59 +0300

    init check customization step

--- src/Element/Section/CheckCustomizations.php
+++ src/Element/Section/CheckCustomizations.php
@@ -47,7 +47,75 @@ class CheckCustomizations implements ElementInterface, RendererAwareInterface, D
     public function getBody(Context $context)
     {
         return $this->renderer->render('check_customizations', [
-            'check_customizations' => $this->dataManager->getListOfCustomizations($context),
+            'check_customizations' => $this->getListOfCustomizations($context),
         ]);
+    }
+
+
+    /**
+     * Get all available SugarCRM versions (sorted ASC).
+     *
+     * @param mixed $flav
+     *
+     * @return mixed
+     */
+    public function getListOfCustomizations($pathToUpgradePackage)
+    {
+        $customizations = array();
+        $customizations['deleted_files'] =  $this->getListOfDeletedFiles($pathToUpgradePackage);
+        $customizations['modified_files'] =  $this->getListOfModifiedFiles($pathToUpgradePackage);
+        $customizations['post_scripts'] =  $this->getListOfScripts($pathToUpgradePackage);
+        $customizations['sql_scripts'] =  $this->getListOfSQLScripts($pathToUpgradePackage);
+
+        return $customizations;
+    }
+
+    /**
+     * Get list of files needs to be deleted
+     *
+     * @param String $pathToUpgradePackage
+     *
+     * @return mixed
+     */
+    public function getListOfDeletedFiles($pathToUpgradePackage)
+    {
+        return '';
+    }
+
+    /**
+     * Get list of files needs to be Modified
+     *
+     * @param String $pathToUpgradePackage
+     *
+     * @return mixed
+     */
+    public function getListOfModifiedFiles($pathToUpgradePackage)
+    {
+        return '';
+    }
+
+
+    /**
+     * Get list of scripts needs to be run for upgrade
+     *
+     * @param String $pathToUpgradePackage
+     *
+     * @return mixed
+     */
+    public function getListOfScripts($pathToUpgradePackage)
+    {
+        return '';
+    }
+
+    /**
+     * Get list of sql migration scripts needs to be run for upgrade
+     *
+     * @param String $pathToUpgradePackage
+     *
+     * @return mixed
+     */
+    public function getListOfSQLScripts($pathToUpgradePackage)
+    {
+        return '';
     }
 }
