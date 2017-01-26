aa01ce98568367a0311554c64209ee3353936e68
Mike Kamornikov <mikekamornikov@gmail.com>
2017-01-26 16:47:44 +0300

    update spec generator spec

--- spec/Spec/GeneratorSpec.php
+++ spec/Spec/GeneratorSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace spec\Sugarcrm\UpgradeSpec\Generator;
+namespace spec\Sugarcrm\UpgradeSpec\Spec;
 
 use Prophecy\Argument;
 use Sugarcrm\UpgradeSpec\Element\ElementInterface;
@@ -8,32 +8,37 @@ use Sugarcrm\UpgradeSpec\Element\Provider;
 use Sugarcrm\UpgradeSpec\Element\Generator as ElementGenerator;
 use Sugarcrm\UpgradeSpec\Formatter\FormatterInterface;
 use PhpSpec\ObjectBehavior;
+use Sugarcrm\UpgradeSpec\Spec\Context;
 use Sugarcrm\UpgradeSpec\Spec\Generator;
 
-class SpecGeneratorSpec extends ObjectBehavior
+class GeneratorSpec extends ObjectBehavior
 {
+    /**
+     * @var Context
+     */
+    private $context;
+
     function let(Provider $elementProvider,
         ElementGenerator $specElementGenerator,
         ElementInterface $step1,
         ElementInterface $step2,
         FormatterInterface $formatter
     ) {
-        $step1->getTitle()->willReturn('step1 title');
-        $step1->getBody()->willReturn('step1 body');
-        $step1->getOrder()->willReturn(1);
-        $step1->isRelevantTo(Argument::cetera())->willReturn(true);
+        $this->context = new Context('7.6.1', '7.8.0.0', 'ULT');
 
-        $step2->getTitle()->willReturn('step2 title');
-        $step2->getBody()->willReturn('step2 body');
-        $step2->getOrder()->willReturn(2);
-        $step2->isRelevantTo(Argument::cetera())->willReturn(true);
+        $step1->getTitle()->willReturn('section1 title');
+        $step1->getBody()->willReturn('section1 body');
 
-        $elementProvider->getElements(Argument::cetera())->willReturn([$step1, $step2]);
+        $step2->getTitle()->willReturn('section2 title');
+        $step2->getBody()->willReturn('section2 body');
 
-        $specElementGenerator->generate($step1)->willReturn('step1');
-        $specElementGenerator->generate($step2)->willReturn('step2');
+        $elementProvider->getElements($this->context)->willReturn([$step1, $step2]);
+
+        $specElementGenerator->generate($step1, $this->context)->willReturn('section1 full');
+        $specElementGenerator->generate($step2, $this->context)->willReturn('section2 full');
 
         $formatter->asTitle(Argument::any())->willReturn('spec_title');
+        $formatter->asBody(Argument::any())->willReturn('spec_body');
         $formatter->getDelimiter()->willReturn('delimiter');
 
         $this->beConstructedWith($elementProvider, $specElementGenerator, $formatter);
@@ -46,15 +51,16 @@ class SpecGeneratorSpec extends ObjectBehavior
 
     function it_generates_uprade_spec()
     {
-        $spec = 'spec_title' . 'delimiter' . 'step1' . 'delimiter' . 'step2';
-        $this->generate('/path/to/sugarcrm/build', 'new_version')->shouldReturn($spec);
+        $spec = 'spec_title' . 'delimiter' . 'section1 full' . 'delimiter' . 'section2 full';
+
+        $this->generate($this->context)->shouldReturn($spec);
     }
 
     function it_executes_all_configured_steps(ElementGenerator $specElementGenerator, ElementInterface $step1, ElementInterface $step2)
     {
-        $specElementGenerator->generate($step1)->shouldBeCalled();
-        $specElementGenerator->generate($step2)->shouldBeCalled();
+        $specElementGenerator->generate($step1, $this->context)->shouldBeCalled();
+        $specElementGenerator->generate($step2, $this->context)->shouldBeCalled();
 
-        $this->generate('/path/to/sugarcrm/build', 'new_version');
+        $this->generate($this->context);
     }
 }
