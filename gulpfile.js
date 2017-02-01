cb9840bacf5caf82836bfa3c4a5b2eca317b8555
Mike Kamornikov <mikekamornikov@gmail.com>
2017-02-01 17:23:40 +0300

    add behat to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,46 @@
-var gulp = require("gulp"),
-    phpspec = require("gulp-phpspec"),
-    notify = require("gulp-notify"),
+var gulp = require('gulp'),
+    phpspec = require('gulp-phpspec'),
+    behat = require('gulp-behat'),
+    notify = require('gulp-notify'),
+    path = require('path'),
     _ = require('lodash');
 
-gulp.task("test", function() {
+gulp.task('phpspec', function() {
     var options = {
         clear: true,
-        formatter: "pretty",
-        verbose: "v",
+        formatter: 'dot',
+        verbose: 'v',
         notify: true,
         noInteraction: true
     };
-    gulp.src("phpspec.yml")
-        .pipe(phpspec("", options))
-        .on("error", notify.onError(testNotification("fail", "phpspec")))
+    gulp.src('phpspec.yml')
+        .pipe(phpspec('', options))
+        .on('error', notify.onError(testNotification('fail', 'phpspec')))
         .pipe(notify(testNotification('pass', 'phpspec')));;
 });
 
-gulp.task("watch", function() {
-    gulp.watch(["spec/**/*.php", "src/**/*.php"], ["test"]);
+gulp.task('behat', function() {
+    var options = {
+        format: 'progress',
+        notify: true
+    };
+    gulp.src('behat.yml')
+        .pipe(behat('', options))
+        .on('error', notify.onError(testNotification('fail', 'behat')))
+        .pipe(notify(testNotification('pass', 'behat')));
 });
 
-gulp.task("default", ["test", "watch"]);
+gulp.task('watch', function() {
+    gulp.watch(['src/**/*.php', 'spec/**/*.php', 'features/**/*.feature'], ['test']);
+});
+
+gulp.task('test', ['phpspec', 'behat']);
+gulp.task('default', ['test', 'watch']);
 
 function testNotification(status, pluginName, override) {
     return _.merge({
         title:   ( status == 'pass' ) ? 'Tests Passed' : 'Tests Failed',
         message: ( status == 'pass' ) ? '\n\nAll tests have passed!\n\n' : '\n\nOne or more tests failed...\n\n',
-        icon:    __dirname + '/node_modules/gulp-' + pluginName +'/assets/test-' + status + '.png'
+        icon:    path.join(__dirname, 'node_modules/gulp-' + pluginName, 'assets/test-' + status + '.png')
     }, override);;
 }
